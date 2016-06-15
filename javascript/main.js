98519bac3ccada8192210be5fdc99091ecfa18eb
painter105 <patrick.schilder@student.uva.nl>
2016-06-15 17:00:21 +0200

    New donut (almost working)

--- javascript/main.js
+++ javascript/main.js
@@ -136,8 +136,165 @@ window.onload = function() {
 		custom_map.arc(plotArray);//, {strokeWidth: 0.1}, arcSharpness: 0.5, strokeColor: '#DD1C77'});
 	};
 
+// donut init
 
-} /* end of window.onload */
+var width = 400,
+    height = 200,
+	radius = Math.min(width, height) / 2;
+
+var svg = d3.select("#donut")
+	.attr("width", width)
+	.attr("height", height)
+	.select("g");
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
+var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) {
+		return d.value;
+	});
+
+var arc = d3.svg.arc()
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var key = function(d){ return d.data.label; };
+
+var color = d3.scale.ordinal()
+	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+function randomData (){
+	var labels = color.domain();
+	return labels.map(function(label){
+		return { label: label, value: Math.random() }
+	});
+};
+
+// -- Donut Pie Chart ---------------------------------
+// from: http://bl.ocks.org/dbuezas/9306799
+
+function drawDonut(countryCode, option) {
+
+	var countryData = csvByCountryOfAsylum[countryCode];
+
+	// count the refugees
+	var pieData = [];
+	
+	countryData.forEach(function(d) {
+		if (d.year == 1975) {
+			pieData.push({label: d.countryOfOrigin , value: +d.refugees});
+		};
+	});
+
+	change(pieData);
+
+}; /* end of drawDonut */
+
+function change(data) {
+	var svg = d3.select("#donut")
+		.select("g");
+	/* ------- PIE SLICES -------*/
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data), key);
+
+	slice.enter()
+		.insert("path")
+		.style("fill", function(d) { return color(d.data.label); })
+		.attr("class", "slice");
+
+	slice		
+		.transition().duration(1000)
+		.attrTween("d", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				return arc(interpolate(t));
+			};
+		})
+
+	slice.exit()
+		.remove();
+
+	/* ------- TEXT LABELS -------*/
+
+	var text = svg.select(".labels").selectAll("text")
+		.data(pie(data), key);
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.text(function(d) {
+			return d.data.label;
+		});
+	
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.transition().duration(1000)
+		.attrTween("transform", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+
+	text.exit()
+		.remove();
+
+	/* ------- SLICE TO TEXT POLYLINES -------*/
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(data), key);
+	
+	polyline.enter()
+		.append("polyline");
+
+	polyline.transition().duration(1000)
+		.attrTween("points", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+				return [arc.centroid(d2), outerArc.centroid(d2), pos];
+			};			
+		});
+	
+	polyline.exit()
+		.remove();
+}; 
+
+}; /* end of window.onload */
 
 
 
@@ -265,65 +422,6 @@ function drawGraph(countryCode, option) {
 
 };
 
-// -- Donut Pie Chart ---------------------------------
-// from: https://bl.ocks.org/mbostock/3887193
-
-function drawDonut(countryCode, option) {
-
-	var countryData = csvByCountryOfAsylum[countryCode];
-
-	// count the refugees
-	var pieData = [];
-	
-	countryData.forEach(function(d) {
-		if (d.year == 1975) {
-			pieData.push({"country": d.countryOfOrigin , "refugees": +d.refugees});
-		};
-	});
 
 
-	var width = 400,
-	    height = 200,
-	    radius = Math.min(width, height) / 2;
 
-	var color = d3.scale.ordinal()
-	    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-	var arc = d3.svg.arc()
-	    .outerRadius(radius - 5)
-	    .innerRadius(radius - 50);
-
-	var pie = d3.layout.pie()
-	    .sort(null)
-	    .value(function(d) { return d.refugees; });
-
-	var svg = d3.select("#donut")
-	    .attr("width", width)
-	    .attr("height", height)
-		.select("g")
-	    	.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-	var g = svg.selectAll(".arc")
-		.data(pie(pieData));
-
-	g.exit()
-		.transition()
-		.duration(300)
-		.remove();
-
-    g.enter()
-    		.append("g")
-				.attr("class", "arc");
-
-	g.append("path")
-		.attr("d", arc)
-		.style("fill", function(d) { return color(d.data.country); });
-
-	g.append("text")
-		.attr("class", "pieText")
-		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-		.attr("dy", ".35em")
-		.text(function(d) { return d.data.country; });
-
-
-};
